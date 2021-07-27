31134d2fa1dc828fa07642546977143ca89cca0d
attrix <lucho.bernal@hotmail.com>
2021-07-27 14:56:09 -0300

    logo

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -5,32 +5,38 @@ import { Component, ElementRef, HostListener, OnInit, Renderer2, ViewChild } fro
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit{
+export class NavBarComponent implements OnInit {
 
-  @ViewChild('nav__logo_img',{static: true}) nav_img: any;
-  @ViewChild('nav__logo',{static: true}) nav_logo: any;
+  @ViewChild('nav__logo_img', { static: true }) nav_img: any;
+  @ViewChild('nav__logo', { static: true }) nav_logo: any;
 
   constructor(private render: Renderer2) {
 
-    
-    
+
   }
   ngOnInit(): void {
     
+    if (window.scrollY == 0) {
+      this.render.setAttribute(this.nav_img.nativeElement, 'style', "visibility:hidden;");
+      this.render.setAttribute(this.nav_logo.nativeElement, 'style', "padding-left:7em;");
+    }
+
   }
 
   @HostListener('window:scroll', ['$event']) onscroll() {
-      if (window.scrollY > 217) {
 
-        this.render.setAttribute(this.nav_img.nativeElement, 'style', "width:3em;position:relative;visibility:none;");
-        this.render.setAttribute(this.nav_logo.nativeElement, 'style', "padding-left:0;");
-      }else if(window.scrollY<217){
-        this.render.setAttribute(this.nav_img.nativeElement, 'style', "visibility:hidden;");
-        this.render.setAttribute(this.nav_logo.nativeElement, 'style', "padding-left:5em;");
-      }
+
+     if (window.scrollY > 217) {
+
+      this.render.setAttribute(this.nav_img.nativeElement, 'style', "width:3em;position:relative;visibility:none;");
+      this.render.setAttribute(this.nav_logo.nativeElement, 'style', "padding-left:0;");
+    } else if (window.scrollY < 217) {
+      this.render.setAttribute(this.nav_img.nativeElement, 'style', "visibility:hidden;");
+      this.render.setAttribute(this.nav_logo.nativeElement, 'style', "padding-left:7em;");
+    }
 
   }
-  
 
-  
+
+
 }
