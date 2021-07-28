dea554953160edb77d4a3089a4cfc1b1065bcd46
LodolaTomas <tomaslodola1@gmail.com>
2021-07-27 22:24:12 -0300

    form contact

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -8,29 +8,61 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class ContactComponent implements OnInit {
 
-  FormData: FormGroup;
+  userForm: FormGroup;
+  private isEmail =/\S+@\S+\.\S+/;
   loading:boolean = false;
 
   constructor(private builder: FormBuilder) { }
 
   ngOnInit() {
-    this.FormData = this.builder.group({
-      name: new FormControl('', [Validators.required]),
-      lastname: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.compose([Validators.required, Validators.email])]),
+    this.userForm = this.builder.group({
+      fullname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.compose([Validators.required,Validators.pattern(this.isEmail)])]),
+      subject:new FormControl('', [Validators.required]),
       comment: new FormControl('', [Validators.required])
     })
   }
 
-  showContact(form:any){
+  onSubmit(){
     this.loading = true;
 
       setTimeout(() => {
         this.loading = false;
-        this.FormData.reset();
+        this.userForm.reset();
       }, 3000);
 
-    console.log(form);
+    console.log(this.userForm);
+  }
+
+ /*  MatchPassword(field: string) {
+    let password = this.userForm.get('password').value;
+    let confirmPassword = this.userForm.get(field).value;
+    const validateField = this.userForm.get('password');
+    const cP = this.userForm.get(field);
+    let passwordMatch: string;
+
+    if (!validateField.valid && validateField.touched && !cP.valid && cP.touched) {
+      passwordMatch = 'is-invalid';
+      cP.setErrors({ passwordMatch: true });
+    } else if (validateField.valid ) {
+      if (password != confirmPassword) {
+        passwordMatch = 'is-invalid';
+        cP.setErrors({ passwordMatch: true });
+      } else {
+        cP.setErrors(null);
+        passwordMatch = 'is-valid';
+      }
+    }
+    return passwordMatch;
+  } */
+
+  isValidField(field: string): string {
+    const validateField = this.userForm.get(field);
+    return !validateField.valid && validateField.touched
+      ? 'is-invalid'
+      : validateField.touched
+      ? 'is-valid'
+      : '';
   }
 
 }
