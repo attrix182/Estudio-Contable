c802d94fc868fa8a85b7d8634593b45613665db5
attrix <lucho.bernal@hotmail.com>
2021-09-04 20:59:34 -0300

    picture

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, TemplateRef, ViewContainerRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -12,26 +13,49 @@ import { ViewPostService } from 'src/app/services/view-post.service';
 })
 export class AdminPanelComponent implements OnInit {
 
-    
+
   @ViewChild('modaladdPost', { read: TemplateRef })
   modaladdPost: TemplateRef<any>
 
-  constructor(private authService: AuthService, private router: Router, private viewPostService: ViewPostService,
-    private modalService: NgbModal,private vref: ViewContainerRef) { }
+  filePath: string;
+  myForm: FormGroup;
 
 
+  constructor(public fb: FormBuilder,private authService: AuthService, private router: Router, private viewPostService: ViewPostService,
+    private modalService: NgbModal, private vref: ViewContainerRef) {
+      this.myForm = this.fb.group({
+        img: [null],
+        filename: ['']
+      })
+     }
 
 
-  ngOnInit(): void {}
+    imagePreview(e) {
+      const file = (e.target as HTMLInputElement).files[0];
+  
+      this.myForm.patchValue({
+        img: file
+      });
+  
+      this.myForm.get('img').updateValueAndValidity()
+  
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.filePath = reader.result as string;
+      }
+      reader.readAsDataURL(file)
+    }
+
+  ngOnInit(): void { }
 
   logOut() {
     this.authService.logOut();
     this.router.navigateByUrl('/');
-    
+
   }
 
 
-  writePost(){
+  writePost() {
     this.modalService.open(this.modaladdPost)
   }
 
