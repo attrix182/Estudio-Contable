bff376896cf243c47ab0d07dbb222d66c9ed3f19
attrix <lucho.bernal@hotmail.com>
2021-09-27 21:06:54 -0300

    fixAUTOPLAY

--- src/app/pages/body/body.component.ts
+++ src/app/pages/body/body.component.ts
@@ -8,12 +8,11 @@ import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/co
 export class BodyComponent implements OnInit {
 
   @ViewChild('nav__logo_img', { static: true }) logo_img: any;
-  @ViewChild('videoPlayer') videoplayer: ElementRef;
-  constructor(private render: Renderer2) {
-  }
+
+  constructor(private render: Renderer2) {}
 
   ngOnInit(): void {
-    this.videoplayer?.nativeElement.play();
+
   }
   hiddeLogo(flag: boolean) {
     if (flag) {
